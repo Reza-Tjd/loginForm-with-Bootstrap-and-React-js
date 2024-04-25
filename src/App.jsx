a025d36ae71bc4c14c914e0687ff62e33c5327ad
Reza <rtajadod93@gmail.com>
2024-04-26 00:12:12 +0200

    deployed-v2

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import Login from "./Login";
 
+import { HashRouter as Router } from "react-router-dom";
+
+
 export default function App() {
   return (
     <div>
-      <Login></Login>
+      <Router>
+        <Login></Login>
+      </Router>
     </div>
   );
 }
